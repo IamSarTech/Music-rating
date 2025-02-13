aadb96098b87e53be113e58552dde1a5f48b6817
Sarthak Sisodia <sarthaksisodia@Sarthaks-MacBook-Air-2.local>
2025-02-13 23:27:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import AlbumPage from './components/AlbumPage';
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <header>
+          <h1>Music Library Hub</h1>
+        </header>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/album/:id" element={<AlbumPage />} />
+        </Routes>
+
+        <footer>
+          &copy; 2024 Music Review Hub | By: RA2211028010136 & RA2211028010093 &RA2211032010072
+        </footer>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
